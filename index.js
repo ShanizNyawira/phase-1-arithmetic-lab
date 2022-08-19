3638b0a5cd029997d573b3719b5a2b079ac6f7df
Shaniz Nyawira <roselynshaniz@gmail.com>
2022-08-19 09:01:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+
+let random = Math.floor (Math.random() + 1);
+
+let num3 = 56;
+let num4 = 13;
+let mod = num3 % num4;
+
+const max = Math.max (7,13,19,20);
